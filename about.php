1fe0f7cfa3ee28717f84435146f23aee5d681fdf
plamenna.petrova <plamennavp@abv.bg>
2024-01-18 21:50:40 +0200

    Meta information update

--- about.php
+++ about.php
@@ -10,8 +10,8 @@ if (file_exists('./xml/class-outfitters.xml')) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="description" content="Class OutfittersTemplate">
+    <meta name="keywords" content="Class Outfitters unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Class Outfitters</title>
--- index.php
+++ index.php
@@ -35,8 +35,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="description" content="Class Outfitters Template">
+    <meta name="keywords" content="Class Outfitters, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Class Outfitters</title>
